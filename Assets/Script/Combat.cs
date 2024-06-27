14929edb5f6fc8d32fb8c2e987c955d537099738
-Rd <rodrigo.maturanaa@estudiantes.arcos.cl>
2024-06-27 09:23:02 -0400

    Combo

--- Assets/Script/Combat.cs
+++ Assets/Script/Combat.cs
@@ -13,6 +13,9 @@ public class Combat : MonoBehaviour
     public float attackCooldown = 1f; 
     private float lastAttackTime;
 
+
+    private int comboState = 0; 
+
     void Update()
     {
         {
@@ -29,9 +32,28 @@ public class Combat : MonoBehaviour
 
     void Attack()
     {
-        animator.SetTrigger("Attack1");
-
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        
+            switch (comboState)
+            {
+                case 0:  
+                    animator.SetTrigger("Attack1");
+                    lastAttackTime = Time.time;
+                    comboState = 1;
+                    break;
+                case 1:  
+                    animator.SetTrigger("Attack2");
+                    lastAttackTime = Time.time;
+                    comboState = 2;
+                    break;
+                case 2:  
+                    animator.SetTrigger("Attack3");
+                    lastAttackTime = Time.time;
+                    comboState = 0; 
+                    break;
+                default:
+                    break;
+            }
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
 
         foreach(Collider2D enemy in hitEnemies)
         {
